2b35f63cd91db274b33a8f8a68b170430b92165a
dfour <darkdragon_010@hotmail.com>
2019-01-13 16:08:25 +0000

    Tileable Background
    Adding tilable background and follow player camera

---
+++ SpaceFighterTutorial/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject playerObject;  // the player object to follow
+    public float lerpSpeed = 0.5f;
+    private Vector3 offset;
+
+    void Start() {
+        offset = transform.position - playerObject.transform.position;
+    }
+
+    void LateUpdate() {
+        transform.position = Vector3.Lerp(transform.position, playerObject.transform.position + offset, lerpSpeed);
+    }
+}
