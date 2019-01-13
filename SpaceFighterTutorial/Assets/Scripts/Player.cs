893000a35d9f6a4fe84f185089409ae68d01b8d7
dfour <darkdragon_010@hotmail.com>
2019-01-13 14:23:19 +0000

    Player Movement
    Adding Player Movement

---
+++ SpaceFighterTutorial/Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float hitPoints = 100f;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        if(rb == null) {
+            Debug.LogError("Player::Start cant find RigidBody2D </sadface>");
+        }
+    }
+    // Update is called once per frame
+    void Update(){}
+
+    // this is called at a fixed interval for use with physics objects like the RigidBody2D
+    void FixedUpdate() {
+        // check if user has pressed some input keys
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0) {
+
+            // convert user input into world movement
+            float horizontalMovement = Input.GetAxisRaw("Horizontal") * moveSpeed;
+            float verticalMovement = Input.GetAxisRaw("Vertical") * moveSpeed ;
+
+            //assign world movements to a Veoctor2
+            Vector2 directionOfMovement = new Vector2(horizontalMovement, verticalMovement);
+
+            // apply movement to player's transform
+            rb.AddForce(directionOfMovement);
+        }
+    }
+}
